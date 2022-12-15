7a64f6fed6a540b31a2bf03c771fe608b94d431a
andr_ll <andr.lyt.dev@gmail.com>
2022-12-15 23:24:57 +0100

    feat: add rand helpers (#16)

---
+++ src/rand.ts
@@ -0,0 +1,18 @@
+import * as crypto from 'crypto';
+
+export class Rand {
+  bool() {
+    return Math.random() > 0.5;
+  }
+
+  numb(min = 0, max = 10) {
+    return Math.round((max - min) * Math.random()) - min;
+  }
+
+  str(len = 8) {
+    const buf = crypto.randomBytes(len);
+    return buf.toString('hex');
+  }
+}
+
+export const rand = new Rand();
