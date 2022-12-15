e37a437ae3a7a91ef4cdb18622eda4f0a3bd32bd
andr_ll <andr.lyt.dev@gmail.com>
2022-12-15 22:52:30 +0100

    add str

--- src/rand.ts
+++ src/rand.ts
@@ -1,10 +1,17 @@
+import * as crypto from 'crypto';
+
 export class Rand {
     bool() {
         return Math.random() > 0.5;
     }
 
     numb(min = 0, max = 10) {
-        return Math.round((max - mix) * Math.random()) - min
+        return Math.round((max - mix) * Math.random()) - min;
+    }
+
+    str(len = 8) {
+        const buf = crypto.randomBytes(len);
+        return buf.toString('hex');
     }
 }
 
