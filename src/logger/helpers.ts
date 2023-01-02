bb15067069985d49272cd92a70efaad77232c3a7
andr-ii <andr.lyt.dev@gmail.com>
2023-01-02 22:57:31 +0100

    Update helpers.ts

--- src/logger/helpers.ts
+++ src/logger/helpers.ts
@@ -1,3 +1,9 @@
+/**
+ * @description The helpers functions of `logger` utility.
+ * @author Andrii Lytovchenko <andr.lyt.dev@gmail.com>
+ * @licence MIT
+ */
+
 export function getTimeFormatted(): string {
   const [date, year, time] = new Date()
     .toLocaleString(undefined, {
